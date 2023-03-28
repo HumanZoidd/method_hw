f8e8c64af0b49d6bde4c1f757a39aa880c0a7a5f
Sergey Kharitonov <haritonov_s@mail.ru>
2023-03-28 14:38:47 +0400

    git

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 import static java.lang.System.in;
 public class Main {
-    Scanner scanner = new Scanner(in);
     public static void main(String[] args) {
         Scanner scanner = new Scanner(in);
         task1(scanner);
